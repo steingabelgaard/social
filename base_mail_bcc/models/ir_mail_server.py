d82f443c7d99727f8d54411a4e0fe8874f9c3a31
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2022-02-16 11:00:46 +0100

    Issue32647: v12 migration

--- base_mail_bcc/models/ir_mail_server.py
+++ base_mail_bcc/models/ir_mail_server.py
@@ -9,9 +9,9 @@ class IrMailServer(models.Model):
     _inherit = "ir.mail_server"
 
     @api.model
-    def send_email(self, message, mail_server_id=None, smtp_server=None,
-                   smtp_port=None, smtp_user=None, smtp_password=None,
-                   smtp_encryption=None, smtp_debug=False):
+    def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None,
+                   smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False,
+                   smtp_session=None)
         """"Add global bcc email addresses"""
 
         # These are added here in send_email instead of build_email
@@ -36,5 +36,5 @@ class IrMailServer(models.Model):
             message, mail_server_id=mail_server_id, smtp_server=smtp_server,
             smtp_port=smtp_port, smtp_user=smtp_user,
             smtp_password=smtp_password, smtp_encryption=smtp_encryption,
-            smtp_debug=smtp_debug
+            smtp_debug=smtp_debug, smtp_session=smtp_session
         )

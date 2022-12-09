85d199f63e3c687be2652b20523bd0e6cca1ac76
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2022-12-09 12:22:20 +0100

    Issue37299: Fix from address extraction

--- mail_outbound_static/models/ir_mail_server.py
+++ mail_outbound_static/models/ir_mail_server.py
@@ -1,7 +1,7 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 from odoo.tools import formataddr
 
 
@@ -34,8 +34,8 @@ class IrMailServer(models.Model):
             mail_server = self.sudo().search([], order='sequence', limit=1)
 
         if mail_server and mail_server.smtp_from and mail_server.smtp_via:
-            split_from = message['From'].rsplit(' <', 1)
-            if mail_server.whitelisted_email and len(split_from) > 1 and split_from[1] in mail_server.whitelisted_email:
+            split_from = tools.email_split(message['From'])
+            if mail_server.whitelisted_email and len(split_from) == 1 and split_from[0] in mail_server.whitelisted_email:
                 # No rewrite of whitelisted address
                 return super(IrMailServer, self).send_email(
                     message, mail_server_id, smtp_server, *args, **kwargs
